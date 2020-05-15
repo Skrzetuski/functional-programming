161f7057d3d0531265c8d2ed5cde13a57cefac07
Krystian Guminski <krystian.guminsk@gmail.com>
2020-05-16 00:24:32 +0200

    WIP

--- fp/src/fp/euler.clj
+++ fp/src/fp/euler.clj
@@ -3,8 +3,7 @@
 ;;2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 ;;What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-;;TODO: Works for JVM -Xss1024m but I guess it's not suitable xD
-(def smallest-number 2520)  ;;232792560
+(def smallest-number 2520)
 
 (defn inc20
   [number]
@@ -12,14 +11,16 @@
 
 (defn remainder
   [smallest-number]
-  (reduce + (map mod (repeat 20 smallest-number) (range 2 21))))
+  (reduce + (map mod (repeat 20 smallest-number) (range 2 21)))) ;;2->20
 
 (defn euler
-  [smallest-number]
-  (if (= 0 (remainder smallest-number))
-    smallest-number
-    (euler (inc20 smallest-number))))
+  [numb]
+  (if (zero? (remainder numb))
+    (println numb)
+    (recur (inc20 numb)))
+  )
 
 (defn -main
   [& args]
-  (println (euler smallest-number)))
+  (time (euler smallest-number)) ;; 26,5s
+  )
